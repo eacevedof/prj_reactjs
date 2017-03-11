439138389a9f95383e169de866e69d2a54ffd208
Eduardo A. F. (lap_thosiba) <eacevedof@yahoo.es>
2017-03-11 13:34:25 +0100

    db articles en constante

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,5 @@
 /**
- * \app\src\index.js 1.0.2
+ * \app\src\index.js 1.1.0
  * https://youtu.be/1ymoW-mUPYQ?list=PLTUC0zY59vZHMLYBospfQoNrZO-vPvVub&t=918
  * fix: producttablerow
  * https://youtu.be/1ymoW-mUPYQ?list=PLTUC0zY59vZHMLYBospfQoNrZO-vPvVub&t=1168
@@ -8,6 +8,15 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import FilterableProductTable from "./components/FilterableProductTable.js"
+
+const db = [
+    {category:"Sporting goods", price:"$49.99",stocked:true, name:"Football"},
+    {category:"Sporting goods", price:"$9.99",stocked:true, name:"Baseball"},
+    {category:"Sporting goods", price:"29.99",stocked:false, name:"Basketball"},
+    {category:"Electronics", price:"$99.99",stocked:true, name:"iPod Touch"},
+    {category:"Electronics", price:"$399.99",stocked:false, name:"iPhone 5"},
+    {category:"Electronics", price:"$199.99",stocked:true, name:"Nexus 7"},
+]
 
 const eDiv = document.getElementById("divApp")
 ReactDOM.render(<FilterableProductTable/>,eDiv)
