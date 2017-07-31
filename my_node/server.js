6faf6c5b45c419177e7c84f7eeb8df1bde0a4ecc
eacevedof <eacevedof@yahoo.es>
2017-07-31 12:52:55 +0200

    paginas estaticas en 1337

--- my_node/server.js
+++ my_node/server.js
@@ -1,18 +1,8 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-    console.log("request",request,"respones",response)
-    response.simpleJSON = function(codStatus,obj){
-        var sBody = JSON.stringify(obj)
-        response.writeHead(codStatus, {
-            'Content-Type': 'text/plain',
-            'Content-Length': sBody.length,
-            'Access-Control-Allow-Origin' : '*'
-        });
-        response.write(sBody)
-        response.close()
-    }
-    //handler(request,response)
-}).listen(1337);
-
-sys.puts("Server at http://127.0.0.1:1337/");
+//https://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
+var connect = require('connect');
+var serveStatic = require('serve-static')
+const rootpath = __dirname+"/.."
+connect().use(serveStatic(rootpath)).listen(1337, function(){
+    console.log("__dirname",__dirname)
+    console.log('Server running on 1337...')
+})
