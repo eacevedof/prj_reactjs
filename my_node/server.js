efa6aeb3245b66a647d60e4c073b798d433b0266
eacevedof <eacevedof@yahoo.es>
2017-07-31 12:43:42 +0200

    yarn add connect serve-static  instalado

--- my_node/server.js
+++ my_node/server.js
@@ -1,9 +1,18 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin' : '*'
-    });
-    response.end('Hello World\n');
+    console.log("request",request,"respones",response)
+    response.simpleJSON = function(codStatus,obj){
+        var sBody = JSON.stringify(obj)
+        response.writeHead(codStatus, {
+            'Content-Type': 'text/plain',
+            'Content-Length': sBody.length,
+            'Access-Control-Allow-Origin' : '*'
+        });
+        response.write(sBody)
+        response.close()
+    }
+    //handler(request,response)
 }).listen(1337);
+
+sys.puts("Server at http://127.0.0.1:1337/");
