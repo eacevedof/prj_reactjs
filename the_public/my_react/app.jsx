e5d394227e485905790104f74d8216051b1cdd23
eacevedof <eacevedof@yahoo.es>
2017-07-31 14:38:51 +0200

    reloj con jsx const eSpan = <span>{sTime}</span>

--- the_public/my_react/app.jsx
+++ the_public/my_react/app.jsx
@@ -1,13 +1,14 @@
-//app.js 1.0.6
+//app.js 1.0.7
 //https://youtu.be/4gAAiOKOwio?t=880
-console.log("app.js 1.0.6")
+console.log("app.js 1.0.7")
 
 const oDivApp = document.getElementById("app")
 
 //Ejemplo reloj
 function reloj(){
   const sTime = new Date().toLocaleTimeString()
-  oDivApp.textContent = sTime
+  const eSpan = <span>{sTime}</span>
+  ReactDOM.render(eSpan,oDivApp)
 }
 
 setInterval(reloj,1000)
