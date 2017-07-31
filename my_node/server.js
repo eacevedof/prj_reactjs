60a815617166c715e166984bb983c5e56aff0875
eacevedof <eacevedof@yahoo.es>
2017-07-31 12:56:22 +0200

    configurado puerto 8008

--- my_node/server.js
+++ my_node/server.js
@@ -1,8 +1,8 @@
 //https://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
 var connect = require('connect');
 var serveStatic = require('serve-static')
-const rootpath = __dirname+"/.."
-connect().use(serveStatic(rootpath)).listen(1337, function(){
-    console.log("__dirname",__dirname)
-    console.log('Server running on 1337...')
+const publicpath = __dirname+"/.."
+const port = 8008
+connect().use(serveStatic(publicpath)).listen(port, function(){
+    console.log("publicpath:",publicpath,"port:",port)
 })
