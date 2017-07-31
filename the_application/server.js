7ce1d03be41a28529b71fa421c426afbfd989202
eacevedof <eacevedof@yahoo.es>
2017-07-31 13:04:09 +0200

    reorganizo carpetas, the_public, the_application

--- the_application/server.js
+++ the_application/server.js
@@ -1,7 +1,7 @@
 //https://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
 var connect = require('connect');
 var serveStatic = require('serve-static')
-const publicpath = __dirname+"/.."
+const publicpath = __dirname+"/../the_public"
 const port = 8008
 connect().use(serveStatic(publicpath)).listen(port, function(){
     console.log("publicpath:",publicpath,"port:",port)
---
+++ the_public/my_react/app.js
@@ -0,0 +1,2 @@
+//app.js 1.0.0
+console.log("app.js 1.0.0")
