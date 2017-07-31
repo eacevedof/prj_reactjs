20fd4642ee64db454548461904178c449b150b83
eacevedof <eacevedof@yahoo.es>
2017-07-31 13:45:15 +0200

    app.jsx de https://youtu.be/4gAAiOKOwio?t=1009

--- the_public/my_react/app.jsx
+++ the_public/my_react/app.jsx
@@ -1,12 +1,17 @@
-//app.js 1.0.2
+//app.js 1.0.3
 //https://youtu.be/4gAAiOKOwio?t=880
-console.log("app.js 1.0.2")
+console.log("app.js 1.0.3")
 
+//Esto es javascript 
+/*
 const oElement = React.createElement(
   "h1",
   {id:"title"},
   "Hola ",
   React.createElement("span",null,"mundo")
 )
+*/
 
+//Esto es jsx
+const oElement = <h1 id="title">Hola <span>mundo</span></h1>
 ReactDOM.render(oElement,document.getElementById("app"));
