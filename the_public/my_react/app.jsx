0d33c84fe00b170147e6d8aac66b28356d5810e0
eacevedof <eacevedof@yahoo.es>
2017-08-01 15:09:24 +0200

    Componente Saluda a varios

--- the_public/my_react/app.jsx
+++ the_public/my_react/app.jsx
@@ -1,4 +1,4 @@
-//app.js 1.0.8
+//app.js 1.0.9
 //https://youtu.be/ld8pG4RPAeI?t=212
 console.log("app.js 1.0.8")
 
@@ -8,5 +8,12 @@ function Saludar(oProps){
   return <h1>Hola {oProps.name}</h1>
 }
 
-ReactDOM.render(<Saludar name="Eduardo A.F." />,oDivApp)
+ReactDOM.render(
+  <div>
+    <Saludar name="Saludo uno" />
+    <Saludar name="Eduardo A.F." />
+    <Saludar name="Tres" />
+  </div>,
+  oDivApp
+)
 
