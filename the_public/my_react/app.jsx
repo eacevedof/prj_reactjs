53cb167a43bfa39461bb11c3cb3a8a0ceda1e3fe
eacevedof <eacevedof@yahoo.es>
2017-08-02 15:41:17 +0200

    Componentes avatar username y user (como mayor componente)

--- the_public/my_react/app.jsx
+++ the_public/my_react/app.jsx
@@ -1,23 +1,26 @@
-//app.js 1.0.9
-//https://youtu.be/ld8pG4RPAeI?t=353
-console.log("app.js 1.0.9")
+//app.js 1.0.10
+//https://github.com/eacevedof/prj_reactjs/blob/edteam/the_public/my_react/app.jsx
+//https://youtu.be/ld8pG4RPAeI?t=490
+console.log("app.js 1.0.10")
 
 const oDivApp = document.getElementById("app")
 
-const User = oProps =>{
-  return (
-    <div>
-      <img src={oProps.img} alt={oProps.name} width="100" height="100"/>
-      <p>{oProps.name}</p>
+const Avatar = oProps => <img src={oProps.user.img} alt={oProps.user.name} width="100" height="100"/>
+const UserName = oProps => <p>{oProps.user.name}</p>
+const User = oProps => {
+  return(
+    <div className="user-item">
+      <Avatar user={oProps.user} />
+      <UserName user={oProps.user} />
     </div>
   )
 }
 
-ReactDOM.render(
-  <div>
-    <User img="https://s-media-cache-ak0.pinimg.com/originals/00/26/55/002655228ae3e174897bff8980cd50b0.jpg"
-    name="Mario Bros"/>
-  </div>,
-  oDivApp
-)
+const oModelUser = {
+  name: 'Daniel',
+  img: 'https://s-media-cache-ak0.pinimg.com/originals/00/26/55/002655228ae3e174897bff8980cd50b0.jpg'
+}
+
+
+ReactDOM.render(<User user={oModelUser}/>,oDivApp)
 
