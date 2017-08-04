21c4b8d50ed097148c74e176a45694a2b6d231e5
eacevedof <eacevedof@yahoo.es>
2017-08-04 12:41:26 +0200

    no se puede usar import y export pq sin ser precompilados con webpack o browserify

--- the_public/my_react/app.jsx
+++ the_public/my_react/app.jsx
@@ -1,8 +1,10 @@
-//app.js 1.0.13
+//app.js 1.0.14
 //https://github.com/eacevedof/prj_reactjs/blob/edteam/the_public/my_react/app.jsx
 //https://youtu.be/ld8pG4RPAeI?t=1066
 //da error: Uncaught ReferenceError: require is not defined
-console.log("app.js 1.0.13")
+//este error solo se puede solucionar con webpack: 
+//https://stackoverflow.com/questions/36073576/how-to-properly-store-react-components-in-separate-files-and-import-react
+console.log("app.js 1.0.14")
 
 import arUsers from "./models/model_user"
 import UserList from "./components/users/component_user_list"
