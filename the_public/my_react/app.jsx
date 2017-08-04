b63cf5870eb9859577e0bf0de9eb7cfb2422f116
eacevedof <eacevedof@yahoo.es>
2017-08-04 10:48:01 +0200

    browserify instalado para corregir error de importancion en cliente

--- the_public/my_react/app.jsx
+++ the_public/my_react/app.jsx
@@ -1,7 +1,8 @@
-//app.js 1.0.12
+//app.js 1.0.13
 //https://github.com/eacevedof/prj_reactjs/blob/edteam/the_public/my_react/app.jsx
 //https://youtu.be/ld8pG4RPAeI?t=1066
-console.log("app.js 1.0.12")
+//da error: Uncaught ReferenceError: require is not defined
+console.log("app.js 1.0.13")
 
 import arUsers from "./models/model_user"
 import UserList from "./components/users/component_user_list"
