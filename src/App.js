a3fc2a04bfd78083ef3390c78123fba92949d0f1
eacevedof <eacevedof@yahoo.es>
2017-09-14 18:23:29 +0200

    mas formateos

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import ShoppingCart from './components/ShoppingCart';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar inverse staticTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Ecommerce</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
+    render() {
+        return (
+            <div>
+                <Navbar inverse staticTop>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Ecommerce</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                </Navbar>
 
-        <Grid>
-          <Row>
-            <Col sm={8}>
-              <ProductList />
-            </Col>
-            <Col sm={4}>
-              <ShoppingCart />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
-    ) //return
-  }//render
+                <Grid>
+                    <Row>
+                        <Col sm={8}>
+                            <ProductList />
+                        </Col>
+                        <Col sm={4}>
+                            <ShoppingCart />
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        ) //return
+    }//render
 }//App
 
 export default App;
