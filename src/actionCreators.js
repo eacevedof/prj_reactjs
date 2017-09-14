ea13544df796b73d2176db0f841f113cbdadf766
eacevedof <eacevedof@yahoo.es>
2017-09-14 18:27:31 +0200

    mas formateos

---
+++ src/actionCreators.js
@@ -0,0 +1,14 @@
+//actionCreators.js 
+
+const addToCart = oProduct => {
+    return 
+}
+
+const removeFromCart = oProduct => {
+    let oAction = {
+        type: "REMOVE_FROM_CART",
+        product: oProduct
+    }    
+    return oAction
+}//removeFromCart
+
