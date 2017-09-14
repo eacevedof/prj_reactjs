f43a599f0e3c34331b817594d2fc591f99caeff1
eacevedof <eacevedof@yahoo.es>
2017-09-14 18:21:01 +0200

    ProductList.js formateado

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,59 +3,60 @@ import { Button, Glyphicon } from 'react-bootstrap';
 import oStore from "../store"
 
 const styles = {
-  products: {
-    display: 'flex',
-    alignItems: 'stretch',
-    flexWrap: 'wrap'
-  },
-  product: {
-    width: '220px',
-    marginLeft: 10,
-    marginRight: 10
-  }
-};
+    products: {
+        display: 'flex',
+        alignItems: 'stretch',
+        flexWrap: 'wrap'
+    },
+    product: {
+        width: '220px',
+        marginLeft: 10,
+        marginRight: 10
+    }
+}
 
 class ProductList extends Component {
-  constructor() {
-    console.log("new ProductList()")
-    super();
-    this.addToCart = this.addToCart.bind(this);
-    //this.removeFromCart = this.removeFromCart.bind(this)
-    this.state = {
-      products: [
-        { id: 1, name: "Hipster Ultimate", price: 299, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-1.jpg" },
-        { id: 2, name: "On Motion Live", price: 99, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg" },
-        { id: 3, name: "Underground Max", price: 149, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg" },
-      ]
-    }//this.state
-  }//constructor
+    
+    constructor() {
+        console.log("new ProductList()")
+        super();
+        this.addToCart = this.addToCart.bind(this);
+        //this.removeFromCart = this.removeFromCart.bind(this)
+        this.state = {
+            products: [
+                { id: 1, name: "Hipster Ultimate", price: 299, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-1.jpg" },
+                { id: 2, name: "On Motion Live", price: 99, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg" },
+                { id: 3, name: "Underground Max", price: 149, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg" },
+            ]
+        }//this.state
+    }//constructor
 
-  render() {
-    return (
-      <div style={styles.products}>
-        {this.state.products.map(product =>
-          <div className="thumbnail" style={styles.product} key={product.id}>
-            <img src={product.image} alt={product.name} />
-            <div className="caption">
-              <h4>{product.name}</h4>
-              <p>
-                <Button bsStyle="primary" onClick={() => this.addToCart(product)} role="button" disabled={product.inventory <= 0}>
-                  ${product.price} <Glyphicon glyph="shopping-cart" />
-                </Button>
-              </p>
+    render() {
+        return (
+            <div style={styles.products}>
+                {this.state.products.map(product =>
+                    <div className="thumbnail" style={styles.product} key={product.id}>
+                        <img src={product.image} alt={product.name} />
+                        <div className="caption">
+                            <h4>{product.name}</h4>
+                            <p>
+                                <Button bsStyle="primary" onClick={() => this.addToCart(product)} role="button" disabled={product.inventory <= 0}>
+                                  ${product.price} <Glyphicon glyph="shopping-cart" />
+                                </Button>
+                            </p>
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    )//return
-  }//render
+        )//return
+    }//render
 
-  addToCart(oProduct) {
-    oStore.dispatch({
-      type: "ADD_TO_CART",
-      product: oProduct
-    })
-  }//addToCart
+    addToCart(oProduct) {
+        oStore.dispatch({
+            type: "ADD_TO_CART",
+            product: oProduct
+        })
+    }//addToCart
 
 }//ProductList
 
