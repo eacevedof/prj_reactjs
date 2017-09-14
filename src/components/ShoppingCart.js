2151e2250c1f78cdcb87649cace6702b9dd0b0d4
eacevedof <eacevedof@yahoo.es>
2017-09-14 18:17:05 +0200

    ShoppingCart.js formateado antes de usar action creators

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -3,66 +3,66 @@ import { Panel, Table, Button, Glyphicon } from 'react-bootstrap';
 import oStore from "../store"
 
 const oStyles = {
-  footer: {
-    fontWeight: 'bold'
-  }
+    footer: {
+        fontWeight: 'bold'
+    }
 }//oStyles
 
 class ShoppingCart extends Component {
-  constructor() {
-    super();
-    this.removeFromCart = this.removeFromCart.bind(this);
+    
+    constructor() {
+        super();
+        this.removeFromCart = this.removeFromCart.bind(this);
 
-    this.state = {
-      arCart: []
-    }//this.state
+        this.state = {
+          arCart: []
+        }//this.state
 
-    oStore.subscribe(()=>{
-      console.log("oStore.subscribe.callback")
-      this.setState({
-        arCart: oStore.getState().arCart
-      })//this.setState
-    })//store.subscribe
+        oStore.subscribe(()=>{
+          console.log("oStore.subscribe.callback")
+          this.setState({
+            arCart: oStore.getState().arCart
+          })//this.setState
+        })//store.subscribe
+    }//constructor
 
-  }//constructor
+    render() {
+        return (
+            <Panel header="Shopping Cart">
+                <Table fill>
+                    <tbody>
+                      {this.state.arCart.map(product =>
+                        <tr key={product.id}>
+                            <td>{product.name}</td>
+                            <td className="text-right">${product.price}</td>
+                            <td className="text-right">
+                                <Button bsSize="xsmall" bsStyle="danger" onClick={() => this.removeFromCart(product)}>
+                                    <Glyphicon glyph="trash" />
+                                </Button>
+                            </td>
+                        </tr>
+                      )}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan="4" style={oStyles.footer}>
+                              Total: ${this.state.arCart.reduce((sum, product) => sum + product.price, 0)}
+                            </td>
+                        </tr>
+                    </tfoot>
+                </Table>
+            </Panel>
+        )//return
+    }//render
 
-  render() {
-    return (
-      <Panel header="Shopping Cart">
-        <Table fill>
-          <tbody>
-            {this.state.arCart.map(product =>
-              <tr key={product.id}>
-                <td>{product.name}</td>
-                <td className="text-right">${product.price}</td>
-                <td className="text-right">
-                  <Button bsSize="xsmall" bsStyle="danger" onClick={() => this.removeFromCart(product)}>
-                    <Glyphicon glyph="trash" />
-                  </Button>
-                </td>
-              </tr>
-            )}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="4" style={oStyles.footer}>
-                Total: ${this.state.arCart.reduce((sum, product) => sum + product.price, 0)}
-              </td>
-            </tr>
-          </tfoot>
-        </Table>
-
-      </Panel>
-    )//return
-  }//render
-
-  removeFromCart(oProduct){
-    let oAction = {
-      type: "REMOVE_FROM_CART",
-      product: oProduct
-    }
-    oStore.dispatch(oAction)
-  }//removeFromCart
+    //la intención es gestionar esto con action creators
+    removeFromCart(oProduct){
+        let oAction = {
+            type: "REMOVE_FROM_CART",
+            product: oProduct
+        }
+        oStore.dispatch(oAction)
+    }//removeFromCart
 
 }//ShoppingCart
 
