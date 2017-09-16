e87a437af3ecd9224f9c922deb55e72c09ec9470
eacevedof <eacevedof@yahoo.es>
2017-09-16 12:32:35 +0200

    cambio clase ShoppingCart a funcion RenderShoppingCart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React /*, { Component }*/ from 'react'; //Nos sobra Component al convertir a función la clase
 import { Panel, Table, Button, Glyphicon } from 'react-bootstrap';
 import {acRemoveFromCart} from "../actionCreators"
 //react-redux: crea por nosotros un componente contenedor "<Provider>" que envuelve nuestro componente y le pasa 
 //todo por propiedades (estado y metodos que necesitemos)
 //nos ayuda a crear componentes funcionales (en vez de clases)
 //https://youtu.be/dAm3jicYvR8?t=225 Indica lo que sobra despues de usar react-redux
+//https://youtu.be/dAm3jicYvR8?t=407 cambiar clases a funciones
 import {connect} from "react-redux"
 
 console.log("ShoppingCart.js")
@@ -15,38 +16,38 @@ const oStyles = {
     }
 }//oStyles
 
-class ShoppingCart extends Component {
-    
-    render() {
-        console.log("ShoppingCart.render")
-        return (
-            <Panel header="Shopping Cart">
-                <Table fill>
-                    <tbody>
-                      {this.props.arCart.map(oProduct =>
-                        <tr key={oProduct.id}>
-                            <td>{oProduct.name}</td>
-                            <td className="text-right">${oProduct.price}</td>
-                            <td className="text-right">
-                                <Button bsSize="xsmall" bsStyle="danger" onClick={() => this.props.removeFromCart(oProduct)}>
-                                    <Glyphicon glyph="trash" />
-                                </Button>
-                            </td>
-                        </tr>
-                      )}
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td colSpan="4" style={oStyles.footer}>
-                              Total: ${this.props.arCart.reduce((sum,oProduct) => sum + oProduct.price, 0)}
-                            </td>
-                        </tr>
-                    </tfoot>
-                </Table>
-            </Panel>
-        )//return
-    }//render
+//Este archivo ahora se convierte en un componente presentacional.
+//no tiene lógica
+const fnRenderShoppingCart = (oProps)=>{
 
+    console.log("fnShoppingCart")
+    return (
+        <Panel header="Shopping Cart">
+            <Table fill>
+                <tbody>
+                  {oProps.arCart.map(oProduct =>
+                    <tr key={oProduct.id}>
+                        <td>{oProduct.name}</td>
+                        <td className="text-right">${oProduct.price}</td>
+                        <td className="text-right">
+                            <Button bsSize="xsmall" bsStyle="danger" onClick={() => oProps.removeFromCart(oProduct)}>
+                                <Glyphicon glyph="trash" />
+                            </Button>
+                        </td>
+                    </tr>
+                  )}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="4" style={oStyles.footer}>
+                          Total: ${oProps.arCart.reduce((fSum,oProduct) => fSum + oProduct.price, 0)}
+                        </td>
+                    </tr>
+                </tfoot>
+            </Table>
+        </Panel>
+    )//return
+    
 }//ShoppingCart
 
 //con esto evitamos tener el constuctor y el store
@@ -74,4 +75,4 @@ const mapDispatchToProps = fnDispatch => {
 
 //connect el pasa el estado y las funciones a <Provider>
 //mapStateToProps(oState);mapDispatchToProps(fn)
-export default connect(mapStateToProps,mapDispatchToProps)(ShoppingCart);
+export default connect(mapStateToProps,mapDispatchToProps)(fnRenderShoppingCart);
